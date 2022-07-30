e3c4c88f587a374c493960e58eeecc57dcac327d
Yossi <yossidoctor98@gmail.com>
2022-07-30 20:03:39 +0300

    Fixed a bug (_generated_vehicle would return a None instead of a Vehicle)

--- TrafficSimulator/vehicle_generator.py
+++ TrafficSimulator/vehicle_generator.py
@@ -1,24 +1,24 @@
-from numpy.random import randint
+from random import randint
 
 from .vehicle import Vehicle
 
 
 class VehicleGenerator:
-    def __init__(self, sim, vehicle_rate, paths, ems):
+    def __init__(self, sim, vehicle_rate, paths_dict, ems):
         self._sim = sim
         # todo: make _vehicle_rate adaptive, allow to change mid simulation -
         #  to simulate real-life conditions
-        self._vehicle_rate = vehicle_rate
-        self._paths = paths
         self.is_ems = ems
+        self._vehicle_rate = vehicle_rate
+        self._paths_dict = paths_dict  # {weight: path}
         self._last_added_time = 0
         self._upcoming_vehicle = self._generate_vehicle()
 
     def _generate_vehicle(self):
         """Returns a random vehicle from self.vehicles with random proportions"""
-        total = sum(pair[0] for pair in self._paths)
-        r = randint(1, total + 1)
-        for (weight, path) in self._paths:
+        total = sum(weight for weight, path in self._paths_dict)
+        r = randint(0, total)
+        for (weight, path) in self._paths_dict:
             r -= weight
             if r <= 0:
                 first_road = self._sim.roads[path[0]]
@@ -27,17 +27,18 @@ class VehicleGenerator:
     def update(self, vehicle_index):
         """Adds a vehicle"""
         vehicle_generated = False
-        # If time elapsed after last generation is greater than vehicle period,
-        # or there's no vehicles on the map generate a vehicle
+        # If there's no vehicles on the map, or if the time elapsed after last
+        # generation is greater than the vehicle rate, generate a vehicle
         if not self._sim.n_vehicles_generated or self._sim.t - self._last_added_time >= 60 / self._vehicle_rate:
             road = self._sim.roads[self._upcoming_vehicle.path[0]]
             if len(road.vehicles) == 0 or \
                     road.vehicles[-1].x > self._upcoming_vehicle.s0 + self._upcoming_vehicle.length:
                 # If there is space for the generated vehicle; add it
                 self._upcoming_vehicle.time_added = self._sim.t
-                self._upcoming_vehicle.index = vehicle_index
+                self._upcoming_vehicle.index = vehicle_index  # for debugging purposes, todo: remove after completion
                 road.vehicles.append(self._upcoming_vehicle)
                 self._last_added_time = self._sim.t
                 vehicle_generated = True
+            # Prepare a vehicle for the next generation
             self._upcoming_vehicle = self._generate_vehicle()
         return vehicle_generated
