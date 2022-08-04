0bd4aad4cbf0d311c8512d59a8d3f44c41ed4689
Yossi <yossidoctor98@gmail.com>
2022-08-04 16:55:04 +0300

    Temp commit

--- ReinforcementLearning/environment.py
+++ ReinforcementLearning/environment.py
@@ -10,7 +10,6 @@ class Environment:
         self.generation_limit = 50
         self.window = Window(**WINDOW_ARGS)
         self.action_space = Discrete(2)  # TODO: remove Discrete action space
-        self.state_offset = 72
 
     def step(self, step_action, training=False):
         # Applies the action and runs a single simulation update cycle.
@@ -19,7 +18,7 @@ class Environment:
         new_state = self._get_new_state()
 
         # A calculated reward based on pre-defined parameters.
-        step_reward = self._calculate_reward(new_state)
+        step_reward = self._calculate_reward(new_state, step_action)
 
         # Whether a terminal state (as defined under the MDP of the task) is reached.
         terminated = self.window.sim.completed or self.window.sim.collision_detected
@@ -32,27 +31,34 @@ class Environment:
 
     def _get_new_state(self):
         # TODO: ADD DOCS
-        new_state = []
-        for i in range(4):
-            road = self.window.sim.roads[i]
-            signal_state = road.traffic_signal_state
-            standing_vehicles = [vehicle for vehicle in road.vehicles if vehicle.is_stopped]
-            n_standing_vehicles = len(standing_vehicles)
-            has_standing_ems = any(vehicle.is_ems for vehicle in standing_vehicles)
-            new_state.append((signal_state, n_standing_vehicles, has_standing_ems))
-        return tuple(new_state)
+        # for group in [[0, 2], [1, 3]]
+        green_roads, red_roads, cycle = self.window.sim.get_green_light_roads()
+        return tuple(green_roads), tuple(red_roads), cycle
 
-    def _calculate_reward(self, new_state):
+    def _calculate_reward(self, new_state, step_action):
+        if step_action:
+            return -self.window.sim.risk_factor  # if switch and there was a risk factor, bad. if there was no risk, its fine cuz its 0 and fine.
+        return 0
+        # risk_factor = sum(risk_factor for x, y, z, risk_factor in new_state)
+        # civ_factor = 5 * sum(n_standing_vehicles for x, n_standing_vehicles, z, w in new_state)
+        # ems_factor = 10 * sum(n_standing_ems_vehicles for x, y, n_standing_ems_vehicles, w in new_state)
+        # # print(ems_factor)
+        # return -risk_factor - ems_factor - civ_factor
+
         # TODO: ADD DOCS
-        step_reward = 0
+        # step_reward = -self.window.sim.risk_factor
+        # # print(step_reward)
+        #
 
-        step_reward += self.window.sim.passed_green_signal  # TODO: ENCAPSULATE SIM ATTRIBUTE
-        self.window.sim.passed_green_signal = 0  # Reset counter
-        step_reward -= sum(standing_vehicles for x, standing_vehicles, z in new_state)
-        step_reward -= sum(has_standing_ems for x, y, has_standing_ems in new_state) * 2
-        if self.window.sim.collision_detected:
-            step_reward -= 150
-        return step_reward
+        # # step_reward += self.window.sim.passed_green_signal  # TODO: ENCAPSULATE SIM ATTRIBUTE
+        # # self.window.sim.passed_green_signal = 0  # Reset counter
+        # test = sum(standing_vehicles for x, standing_vehicles, z in new_state)
+        # print(test)
+        # step_reward -= sum(standing_vehicles for x, standing_vehicles, z in new_state)
+        # # step_reward -= sum(has_standing_ems for x, y, has_standing_ems in new_state) * 2
+        # if self.window.sim.collision_detected:
+        #     step_reward -= 150
+        # return step_reward
 
     def render(self):
         """
