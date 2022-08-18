8964d21c3378df610bbce4f3a6c407634e722114
Yossi <yossidoctor98@gmail.com>
2022-08-18 17:43:58 +0300

    Updated main args parsing

--- main.py
+++ main.py
@@ -5,25 +5,24 @@ from ReinforcementLearning import q_learning
 
 if __name__ == '__main__':
     parser = ArgumentParser(description="AI Traffic Lights Controller")
-    parser.add_argument("--type",
-                        help="Type of method to run",
+    parser.add_argument("-m, --method",
                         choices=['fc', 'lqf', 'qlearning', 'search'],
                         required=True)
-    parser.add_argument("--episodes", metavar='N',
+    parser.add_argument("-e, --episodes", metavar='N',
                         help="Number of evaluation episodes to run",
                         type=int,
                         required=True)
-    parser.add_argument("--render",
+    parser.add_argument("-r", "--render",
                         help="Displays the simulation window",
                         action='store_true')
     args = parser.parse_args()
-    if args.type == 'fc':
+    if args.method == 'fc':
         action_func = 'fixed_cycle_action'
         default_cycle(n_episodes=args.episodes, action_func_name=action_func, render=args.render)
-    elif args.type == 'lqf':
+    elif args.method == 'lqf':
         action_func = 'longest_queue_action'
         default_cycle(n_episodes=args.episodes, action_func_name=action_func, render=args.render)
-    elif args.type == 'qlearning':
+    elif args.method == 'qlearning':
         q_learning(n_episodes=args.episodes, render=args.render)
-    elif args.type == 'search':
+    elif args.method == 'search':
         pass
